6cc40f9f49c557c99eb85955e48bb491944d192f
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2025-12-26 23:56:12 +0530

    Create database and tables with sample data
    Added SQL commands to create a database and tables for customers and orders, along with sample data inserts. Updated join queries and renamed a column in the customers table.

--- sql_practice.sql
+++ sql_practice.sql
@@ -236,7 +236,7 @@ select count(code), count(distinct code) from country;
 
 -- join (it is used to connect two tables from primary key and foregin key)
 
-select cy.id, cy.name, cy.countrycode, cnt.code, cnt.name, cnt.continent from city as cy
+select cy.id, cy.name, cy.countrycode, cnt.code, cnt.name, cnt.continent from city as ct
 join
 country as cnt
 where cy.countrycode=cnt.code;
@@ -249,4 +249,77 @@ select ct.name, ct.population, cuy.name, cuy.governmentform from city as ct
 join 
 country as cuy
 where ct.countrycode=cuy.code;
+-- get the country name and population and the language spoken with the percentage of each language  
 
+CREATE DATABASE REGEX;
+
+USE REGEX;CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    amount DECIMAL(10,2)
+);
+
+INSERT INTO customers (customer_id, customer_name, city) VALUES
+(1, 'Alice', 'New York'),
+(2, 'Bob', 'Los Angeles'),
+(3, 'Charlie', 'Chicago'),
+(4, 'David', 'Houston'),
+(5, 'Eva', 'Phoenix'),
+(6, 'Frank', 'Philadelphia'),
+(7, 'Grace', 'San Antonio'),
+(8, 'Henry', 'San Diego'),
+(9, 'Ivy', 'Dallas'),
+(10, 'Jack', 'San Jose');
+
+
+INSERT INTO orders (order_id, customer_id, order_date, amount) VALUES
+(101, 1, '2024-01-05', 250.00),
+(102, 2, '2024-01-06', 150.00),
+(103, 3, '2024-01-07', 300.00),
+(104, 1, '2024-01-10', 120.00),
+(105, 5, '2024-01-12', 450.00),
+(106, 6, '2024-01-15', 200.00),
+(107, 2, '2024-01-18', 175.00),
+(108, 8, '2024-01-20', 500.00),
+(109, 11, '2024-01-22', 90.00),
+(110, 12, '2024-01-25', 60.00);
+
+
+
+select * from customers;
+select * from orders;
+
+select c.cid, c.customer_name,
+o.order_id, o.customer_id, o.amount from customers as c
+join orders as o 
+where c.cid=o.customer_id;
+
+select c.customer_id, c.customer_name,
+o.order_id, o.customer_id, o.amount from customers as c inner
+join orders as o 
+on c.customer_id=o.customer_id;
+
+select c.customer_id, c.customer_name,
+o.order_id, o.customer_id, o.amount from customers as c left
+join orders as o 
+on c.customer_id=o.customer_id;
+
+select c.customer_id, c.customer_name,
+o.order_id, o.customer_id, o.amount from customers as c right
+join orders as o 
+on c.customer_id=o.customer_id;
+
+alter table customers rename column customer_id to cid;
+desc customers;
+
+select c.customer_name, sum(o.amount) as total_amount
+ from customers c join orders o 
+ on c.cid=o.customer_id
+ group by c.customer_name;
