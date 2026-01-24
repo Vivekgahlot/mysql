9736611ad4ac5fe594923df310a90ebfac9ce43e
vivek gahlot <94837657+Vivekgahlot@users.noreply.github.com>
2026-01-24 21:25:15 +0530

    Add files via upload

--- sql_practice.sql
+++ sql_practice.sql
@@ -905,3 +905,69 @@ from employees;
 select *,
 rank() over(order by hire_date)
 from employees;
+
+select *,
+rank() over(order by city)
+from employees;
+
+-- dense rank , row number , rank; 
+-- dense rank => isme rank skip nhi hoti h agr rank same h toh
+
+select *,
+dense_rank() over(partition  by department order by salary) from employees;
+
+
+CREATE DATABASE workshop_db;
+
+USE workshop_db;
+
+CREATE TABLE events (
+  event_id   INT PRIMARY KEY,
+  event_name VARCHAR(100) NOT NULL,
+  city       VARCHAR(50)  NOT NULL
+);
+
+CREATE TABLE ticket_sales (
+  sale_id          INT PRIMARY KEY,
+  event_id         INT NOT NULL,
+  sale_date        DATE NOT NULL,
+  ticket_type      VARCHAR(20) NOT NULL,
+  qty              INT NOT NULL,
+  price_per_ticket INT NOT NULL,
+  CONSTRAINT fk_ticket_sales_event
+    FOREIGN KEY (event_id) REFERENCES events(event_id)
+);
+
+INSERT INTO events (event_id, event_name, city) VALUES
+(1, 'Music Fest', 'Mumbai'),
+(2, 'Tech Summit', 'Bengaluru'),
+(3, 'Food Carnival', 'Delhi'),
+(4, 'Startup Meetup', 'Mumbai');
+
+INSERT INTO ticket_sales
+(sale_id, event_id, sale_date, ticket_type, qty, price_per_ticket) VALUES
+(101, 1, '2025-01-05', 'Regular', 2, 1500),
+(102, 1, '2025-01-10', 'VIP',     1, 5000),
+(103, 2, '2025-02-03', 'Regular', 3, 2000),
+(104, 2, '2025-02-10', 'VIP',     1, 6000),
+(105, 3, '2025-03-01', 'Regular', 5,  800),
+(106, 3, '2025-03-15', 'VIP',     2, 2500),
+(107, 4, '2025-01-20', 'Regular', 4, 1200),
+(108, 4, '2025-02-05', 'Regular', 1, 1200);
+
+
+SELECT * FROM events ORDER BY event_id;
+
+SELECT * FROM ticket_sales ORDER BY sale_id;
+
+SHOW TABLES;
+
+-- Find the total quantity sold per event_id.
+select event_id,sum(qty)
+FROM ticket_sales
+GROUP BY event_id;
+
+-- Find the total revenue per event_id.
+select event_id,sum(qty*price_per_ticket)
+from ticket_sales
+group by event_id;
